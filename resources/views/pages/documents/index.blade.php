6e3ec64914f4ace4bfb7c71a37dccffc6d212092
hanhafizh <farhanhafizh770@gmail.com>
2025-03-01 11:15:29 +0700

    docs: button data baru

--- resources/views/pages/documents/index.blade.php
+++ resources/views/pages/documents/index.blade.php
@@ -1,19 +1,18 @@
 <x-app-layout>
     <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
 
-        <!-- Dashboard actions -->
-        <div class="sm:flex sm:justify-between sm:items-center mb-8">
-
+        <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-8">
             <!-- Left: Title -->
-            <div class="mb-4 sm:mb-0">
-                <h1 class="text-2xl md:text-3xl text-gray-800 dark:text-gray-100 font-bold">Dokumen Pengadaan</h1>
+            <h1 class="text-2xl md:text-3xl text-gray-800 dark:text-gray-100 font-bold">
+                Dokumen Pengadaan
+            </h1>
+
+            <!-- Right: Buttons -->
+            <div class="flex gap-2 mt-4 sm:mt-0">
+                <x-button.button-action color="violet" type="button" onclick="window.location=''">
+                    + Data Baru
+                </x-button.button-action>
             </div>
-
-            <!-- Right: Actions -->
-            <div class="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-
-            </div>
-
         </div>
 
         <!-- Cards -->
