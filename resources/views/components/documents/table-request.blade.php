7e7548bbb9f88ed104ea1bd7e409bb79c0576245
hanhafizh <farhanhafizh770@gmail.com>
2025-03-10 13:24:55 +0700

    work_request: Tidak ada permintaan barang yang tersedia

--- resources/views/components/documents/table-request.blade.php
+++ resources/views/components/documents/table-request.blade.php
@@ -75,7 +75,7 @@
                     @else
                         <tr>
                             <td class="p-2 whitespace-nowrap" colspan="9">
-                                <div class="text-center">Tidak ada data yang tersedia.</div>
+                                <div class="text-center">Tidak ada permintaan barang yang tersedia.</div>
                             </td>
                         </tr>
                     @endif
