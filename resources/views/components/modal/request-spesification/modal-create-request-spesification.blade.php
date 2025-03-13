c87d86e88cc61c1e779d75bfec7ddafd09d664b2
hanhafizh <farhanhafizh770@gmail.com>
2025-03-13 22:31:47 +0700

    spec: benarkan teks pop up spec

--- resources/views/components/modal/request-spesification/modal-create-request-spesification.blade.php
+++ resources/views/components/modal/request-spesification/modal-create-request-spesification.blade.php
@@ -4,13 +4,13 @@
 <div x-data="{ modalOpen: false }">
     <!-- Button to Open Modal -->
     <x-button.button-action @click="modalOpen = true" color="teal" type="button">
-        + Rab
+        + Spesification
     </x-button.button-action>
     <div class="fixed inset-0 bg-gray-900 bg-opacity-30 z-50 flex items-center justify-center" x-show="modalOpen" x-cloak>
         <div class="bg-white dark:bg-gray-800 p-8 rounded-lg shadow-lg max-w-lg w-full"
             @click.outside="modalOpen = false">
             <div class="flex justify-center items-center">
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-100 mb-4">Tambah Rab</h3>
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-gray-100 mb-4">Tambah Spesification</h3>
             </div>
             <form action="{{ route('work_request.work_spesifications.store', ['id' => $workRequest->id]) }}"
                 method="POST">
@@ -31,7 +31,7 @@
                         <x-label for="contract_type" value="{{ __('Tipe Kontrak') }}" />
                         <x-input id="contract_type" type="text" name="contract_type"
                             class="mt-1 block w-full min-h-[40px]" required autocomplete="contract_type"
-                            placeholder="Masukkan keterangan" value="" />
+                            placeholder="Masukkan tipe kontrak" value="" />
                         <x-input-error for="contract_type" class="mt-2" />
                     </div>
 
@@ -40,7 +40,7 @@
                         <x-label for="safety_aspect" value="{{ __('Aspek Keamanan') }}" />
                         <x-input id="safety_aspect" type="text" name="safety_aspect"
                             class="mt-1 block w-full min-h-[40px]" required autocomplete="safety_aspect"
-                            placeholder="Masukkan jumlah" value="" />
+                            placeholder="Masukkan aspek keamanan" value="" />
                         <x-input-error for="safety_aspect" class="mt-2" />
                     </div>
 
@@ -48,7 +48,7 @@
                     <div>
                         <x-label for="reporting" value="{{ __('Laporan') }}" />
                         <x-input id="reporting" type="text" name="reporting" class="mt-1 block w-full min-h-[40px]"
-                            required autocomplete="reporting" placeholder="Masukkan satuan" value="" />
+                            required autocomplete="reporting" placeholder="Masukkan laporan" value="" />
                         <x-input-error for="reporting" class="mt-2" />
                     </div>
 
@@ -57,7 +57,7 @@
                         <x-label for="provider_requirements" value="{{ __('Kebutuhan Penyedia') }}" />
                         <x-input id="provider_requirements" type="number" name="provider_requirements"
                             class="mt-1 block w-full min-h-[40px]" required autocomplete="provider_requirements"
-                            placeholder="Masukkan jumlah" value="" />
+                            placeholder="Masukkan kebutuhan penyedia" value="" />
                         <x-input-error for="provider_requirements" class="mt-2" />
                     </div>
 
@@ -66,7 +66,7 @@
                         <x-label for="payment_procedures" value="{{ __('Mekanisme Pembayaran') }}" />
                         <x-input id="payment_procedures" type="text" name="payment_procedures"
                             class="mt-1 block w-full min-h-[40px]" required autocomplete="payment_procedures"
-                            placeholder="Masukkan satuan" value="" />
+                            placeholder="Masukkan mekanisme pembayaran" value="" />
                         <x-input-error for="payment_procedures" class="mt-2" />
                     </div>
                 </div>
