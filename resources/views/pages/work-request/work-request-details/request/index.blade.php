9247456572ccb4c773cbd85b494702ad414ae868
hanhafizh <farhanhafizh770@gmail.com>
2025-03-18 13:12:41 +0700

    work request: responsive informasi dokumen

--- resources/views/pages/work-request/work-request-details/request/index.blade.php
+++ resources/views/pages/work-request/work-request-details/request/index.blade.php
@@ -16,61 +16,41 @@
     </div>
     <div class="mt-4 md:mt-4 md:col-span-2">
         <div class="px-4 py-5 sm:p-6 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-
-            <div class="grid grid-cols-2 grid-rows-5 gap-4">
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <div>
-                    <div>
-                        <strong>Bagian / Divisi:</strong> {{ $workRequest->department }}
-                    </div>
+                    <strong>Bagian / Divisi:</strong> {{ $workRequest->department }}
                 </div>
                 <div>
-                    <div>
-                        <strong>Nomor:</strong> {{ $workRequest->request_number }}
-                    </div>
+                    <strong>Nomor:</strong> {{ $workRequest->request_number }}
                 </div>
                 <div>
-                    <div>
-                        <strong>Judul Proyek:</strong> {{ $workRequest->project_title }}
-                    </div>
+                    <strong>Judul Proyek:</strong> {{ $workRequest->project_title }}
                 </div>
                 <div>
-                    <div>
-                        <strong>Tanggal:</strong> {{ $workRequest->request_date }}
-                    </div>
+                    <strong>Tanggal:</strong> {{ $workRequest->request_date }}
                 </div>
                 <div>
-                    <div>
-                        <strong>Pemilik Proyek:</strong> {{ $workRequest->project_owner }}
-                    </div>
+                    <strong>Pemilik Proyek:</strong> {{ $workRequest->project_owner }}
                 </div>
                 <div>
-                    <div>
-                        <strong>Tenggat:</strong> {{ $workRequest->deadline }}
-                    </div>
+                    <strong>Tenggat:</strong> {{ $workRequest->deadline }}
                 </div>
                 <div>
-                    <div>
-                        <strong>No Kontrak:</strong> {{ $workRequest->contract_number }}
-                    </div>
+                    <strong>No Kontrak:</strong> {{ $workRequest->contract_number }}
                 </div>
                 <div>
-                    <div>
-                        <strong>PIC:</strong> {{ $workRequest->pic }}
-                    </div>
+                    <strong>PIC:</strong> {{ $workRequest->pic }}
                 </div>
                 <div>
-                    <div>
-                        <strong>Jenis Pengadaan:</strong> {{ $workRequest->procurement_type }}
-                    </div>
+                    <strong>Jenis Pengadaan:</strong> {{ $workRequest->procurement_type }}
                 </div>
                 <div>
-                    <div>
-                        <strong>Aanwijzing:</strong> {{ $workRequest->aanwijzing }}
-                    </div>
+                    <strong>Aanwijzing:</strong> {{ $workRequest->aanwijzing }}
                 </div>
             </div>
         </div>
     </div>
+
     {{-- End Information Dokumen --}}
 
     <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between mt-8">
