f76f487552ec62cf3b0c23da8e3ed5333b870e98
hanhafizh <farhanhafizh770@gmail.com>
2025-03-18 13:08:01 +0700

    work_request: navbar responsive

--- resources/views/pages/work-request/work-request-details/show.blade.php
+++ resources/views/pages/work-request/work-request-details/show.blade.php
@@ -1,11 +1,11 @@
 <x-app-layout>
-    <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto ">
+    <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
         <!-- Navbar-style tabs -->
         <div class="border-b mb-8">
             <div class="mb-4">
                 <div class="flex justify-between items-center">
-                    <!-- Tabs -->
-                    <div class="flex space-x-8">
+                    <!-- Tabs (Hidden on mobile) -->
+                    <div class="hidden md:flex space-x-8">
                         <a href="{{ route('work_request.work_request_items.show', $workRequest->id) }}"
                             class="text-xl text-gray-800 dark:text-gray-100 pb-2 
                             {{ request()->routeIs('work_request.work_request_items.show') ? 'font-bold border-b-2 border-teal-600' : 'hover:text-teal-600 hover:border-b-2 hover:border-teal-600' }}">
@@ -17,11 +17,41 @@
                             {{ request()->routeIs('work_request.work_rabs.show') ? 'font-bold border-b-2 border-teal-600' : 'hover:text-teal-600 hover:border-b-2 hover:border-teal-600' }}">
                             Form RAB
                         </a>
+
                         <a href="{{ route('work_request.work_spesifications.show', $workRequest->id) }}"
                             class="text-xl text-gray-800 dark:text-gray-100 pb-2 
                             {{ request()->routeIs('work_request.work_spesifications.show') ? 'font-bold border-b-2 border-teal-600' : 'hover:text-teal-600 hover:border-b-2 hover:border-teal-600' }}">
                             Form Spesification
                         </a>
+                    </div>
+
+                    <!-- Dropdown Menu for Mobile -->
+                    <div class="md:hidden relative" x-data="{ open: false }">
+                        <button @click="open = !open"
+                            class="text-xl text-gray-800 dark:text-gray-100 pb-2 flex items-center">
+                            Menu
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path fill-rule="evenodd"
+                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </button>
+                        <div x-show="open" @click.away="open = false"
+                            class="absolute left-0 mt-2 w-48 bg-white dark:bg-gray-800 shadow-md rounded-lg py-2 z-50">
+                            <a href="{{ route('work_request.work_request_items.show', $workRequest->id) }}"
+                                class="block px-4 py-2 text-gray-800 dark:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-700">
+                                Form Request
+                            </a>
+                            <a href="{{ route('work_request.work_rabs.show', $workRequest->id) }}"
+                                class="block px-4 py-2 text-gray-800 dark:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-700">
+                                Form RAB
+                            </a>
+                            <a href="{{ route('work_request.work_spesifications.show', $workRequest->id) }}"
+                                class="block px-4 py-2 text-gray-800 dark:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-700">
+                                Form Spesification
+                            </a>
+                        </div>
                     </div>
 
                     <!-- Actions -->
