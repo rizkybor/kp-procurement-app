5375e28413366b35805d1868145125024c35bedd
hanhafizh <farhanhafizh770@gmail.com>
2025-05-14 14:14:32 +0700

    tables: fix

--- resources/views/pages/work-request/index.blade.php
+++ resources/views/pages/work-request/index.blade.php
@@ -145,6 +145,12 @@
                 dom: 'rtip',
                 pagingType: "simple",
                 responsive: true,
+                language: {
+                    paginate: {
+                        previous: '',
+                        next: '',
+                    }
+                },
                 columns: [{
                         data: 'id',
                         name: 'id',
@@ -334,19 +340,19 @@
             <nav class="flex" role="navigation" aria-label="Navigation">
                 <div class="mr-2">
                     ${currentPage > 1 ? `
-                                                                    <button data-page="${currentPage - 2}" 
-                                                                        class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
-                                                                        border border-gray-200 dark:border-gray-700/60 text-gray-600 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-900 shadow-sm">
-                                                                        <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
-                                                                            <path d="M9.4 13.4l1.4-1.4-4-4 4-4-1.4-1.4L4 8z" />
-                                                                        </svg>
-                                                                    </button>` : `
-                                                                    <span class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
-                                                                        border border-gray-200 dark:border-gray-700/60 text-gray-300 dark:text-gray-600 shadow-sm">
-                                                                        <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
-                                                                            <path d="M9.4 13.4l1.4-1.4-4-4 4-4-1.4-1.4L4 8z" />
-                                                                        </svg>
-                                                                    </span>`}
+                                                                            <button data-page="${currentPage - 2}" 
+                                                                                class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
+                                                                                border border-gray-200 dark:border-gray-700/60 text-gray-600 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-900 shadow-sm">
+                                                                                <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
+                                                                                    <path d="M9.4 13.4l1.4-1.4-4-4 4-4-1.4-1.4L4 8z" />
+                                                                                </svg>
+                                                                            </button>` : `
+                                                                            <span class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
+                                                                                border border-gray-200 dark:border-gray-700/60 text-gray-300 dark:text-gray-600 shadow-sm">
+                                                                                <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
+                                                                                    <path d="M9.4 13.4l1.4-1.4-4-4 4-4-1.4-1.4L4 8z" />
+                                                                                </svg>
+                                                                            </span>`}
                 </div>
                 <ul class="inline-flex text-sm font-medium -space-x-px rounded-lg shadow-sm">`;
 
@@ -377,19 +383,19 @@
                 </ul>
                 <div class="ml-2">
                     ${currentPage < totalPages ? `
-                                                            <button data-page="${currentPage}" 
-                                                                class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
-                                                                border border-gray-200 dark:border-gray-700/60 text-gray-600 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-900 shadow-sm">
-                                                                <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
-                                                                    <path d="M6.6 13.4L5.2 12l4-4-4-4 1.4-1.4L12 8z" />
-                                                                </svg>
-                                                            </button>` : `
-                                                            <span class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
-                                                                border border-gray-200 dark:border-gray-700/60 text-gray-300 dark:text-gray-600 shadow-sm">
-                                                                <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
-                                                                    <path d="M6.6 13.4L5.2 12l4-4-4-4 1.4-1.4L12 8z" />
-                                                                </svg>
-                                                            </span>`}
+                                                                    <button data-page="${currentPage}" 
+                                                                        class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
+                                                                        border border-gray-200 dark:border-gray-700/60 text-gray-600 dark:text-gray-300 hover:bg-gray-50 dark:hover:bg-gray-900 shadow-sm">
+                                                                        <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
+                                                                            <path d="M6.6 13.4L5.2 12l4-4-4-4 1.4-1.4L12 8z" />
+                                                                        </svg>
+                                                                    </button>` : `
+                                                                    <span class="inline-flex items-center justify-center rounded-lg leading-5 px-2.5 py-2 bg-white dark:bg-gray-800 
+                                                                        border border-gray-200 dark:border-gray-700/60 text-gray-300 dark:text-gray-600 shadow-sm">
+                                                                        <svg class="fill-current" width="16" height="16" viewBox="0 0 16 16">
+                                                                            <path d="M6.6 13.4L5.2 12l4-4-4-4 1.4-1.4L12 8z" />
+                                                                        </svg>
+                                                                    </span>`}
                 </div>
             </nav>
             </div>`;
